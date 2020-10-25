b7cc59e14997ba5c14905477a6f617fe984f5df8
sleeplesseditor <josh@bryson.me.uk>
2020-10-25 15:58:59 +0000

    Initial setup for Particles background

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js';
+import particlesConfig from './config/configParticles';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ position: 'relative', overflow: "hidden" }}>
+      <div style={{ position: 'absolute'}}>
+        <Particles height="100vh" width="100vw" params={particlesConfig} />
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
